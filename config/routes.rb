c1c830de3c70a8043f64bf60586855993cf9138f
Brandon <brandon@membr.com>
2013-08-15 20:30:30 -0400

    Begin basic layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 FarkleRails::Application.routes.draw do
   
   root to: 'common_pages#home'
+
+  match '/about',   to: 'common_pages#about'
+  match '/contact', to: 'common_pages#contact'
   
 end
--- spec/requests/common_pages_spec.rb
+++ spec/requests/common_pages_spec.rb
@@ -16,4 +16,20 @@ describe "CommonPages" do
     it { should_not have_selector 'title', text: " | Home" }
   end
   
+  describe "About Page" do
+    before { visit about_path }
+    let(:page_title) { 'About' }
+
+    it_should_behave_like "all common pages"
+    it { should have_selector 'h1', text: "So Why Farkle on Rails?" }
+  end
+
+  describe "Contact Page" do
+    before { visit contact_path }
+    let(:page_title) { 'Contact' }
+
+    it_should_behave_like "all common pages"
+    it { should have_selector 'h1', text: "How To Get In Contact With Me" }
+  end
+
 end
